d17d757571a0504f6d87bcf34d340ee2d8517c84
Skandasoft <skandasoftinc@gmail.com>
2021-06-21 21:03:27 -0500

    Add Index/Progress Step - Day 2

---
+++ progress-steps/progress-steps.js
@@ -0,0 +1,42 @@
+let steps = document.querySelectorAll('.circle');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+let progressLine = document.getElementById('progress-line');
+let current = 1;
+
+let update = function () {
+	steps.forEach((step, idx) => {
+		if (idx < current) {
+			step.classList.add('active');
+		} else {
+			step.classList.remove('active');
+		}
+
+		if (current === 1) {
+			prev.disabled = true;
+		} else if (current === steps.length) {
+			next.disabled = true;
+		} else {
+			next.disabled = false;
+			prev.disabled = false;
+		}
+
+		progressLine.style.width = ((current - 1) / (steps.length - 1)) * 100 + '%';
+	});
+};
+
+next.addEventListener('click', (e) => {
+	current++;
+	if (current > steps.length) {
+		current = steps.length;
+	}
+	update();
+});
+
+prev.addEventListener('click', (e) => {
+	current--;
+	if (current < 1) {
+		current = 1;
+	}
+	update();
+});
